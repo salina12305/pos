b8cda993dc6c96a13559d37cc5448ff15c105c26
salina12305 <prakritistha06@gmail.com>
2025-12-04 12:37:11 +0545

    init

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome to the Home Page"});
+});
+app.listen(3000,()=>{
+    console.log("Server running on http://localhost:3000");
+});
